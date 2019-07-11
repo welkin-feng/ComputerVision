d930d7110e6a0fd3c792588ee6a4e56a321ca81c
Welkin <382523558@qq.com>
2019-07-11 18:45:39 +0800

    docs: add Chinese comment

--- train_simple.py
+++ train_simple.py
@@ -144,19 +144,32 @@ def test(test_loader, net, criterion, optimizer, epoch, device):
 
 def start_training(work_path, resume = False, config_dict = None):
     global args, writer, logger, config, best_prec
+
+    # 设置路径 work_path
+    # set work_path
     args = EasyDict({'work_path': work_path, 'resume': resume})
+
+    # 设置event路径
+    # set event path
     writer = SummaryWriter(logdir = args.work_path + '/event')
+
+    # 创建logger用于记录日志
+    # create logger and write to log.txt
     logger = Logger(log_file_name = args.work_path + '/log.txt',
                     log_level = logging.DEBUG, logger_name = "CIFAR").get_log()
 
     if config_dict is None:
-        # read config from yaml file
+        # 从yaml文件中读取配置config
+        # read config dict from yaml file
         with open(args.work_path + '/config.yaml') as f:
             config_dict = yaml.load(f)
-    # convert to dict
+
+    # 将config转换成EasyDict
+    # convert config dict to EasyDict
     config = EasyDict(config_dict)
     logger.info(config)
 
+    # 创建网络模型
     # define netowrk
     net = get_model(config)
     logger.info(net)
@@ -170,8 +183,12 @@ def start_training(work_path, resume = False, config_dict = None):
         cudnn.benchmark = True
     net.to(device)
 
-    # define loss and optimizer
+    # 设置loss计算函数
+    # define loss
     criterion = nn.CrossEntropyLoss()
+
+    # 设置optimizer用于反向传播梯度
+    # define optimizer
     if config.optimize.type == 'SGD':
         optimizer = optim.SGD(net.parameters(), lr = config.lr_scheduler.base_lr,
                               momentum = config.optimize.momentum,
@@ -184,6 +201,7 @@ def start_training(work_path, resume = False, config_dict = None):
         optimizer = optim.RMSprop(net.parameters(), lr = config.lr_scheduler.base_lr,
                                   weight_decay = config.optimize.weight_decay)
 
+    # 从checkpoint中恢复网络模型
     # resume from a checkpoint
     last_epoch = -1
     best_prec = 0
@@ -193,17 +211,24 @@ def start_training(work_path, resume = False, config_dict = None):
             best_prec, last_epoch = load_checkpoint(
                 ckpt_file_name, net, optimizer = optimizer)
 
-    # load training data, do data augmentation and get data loader
+    # 加载训练数据 并进行数据扩增
+    # load training data & do data augmentation
     transform_train = transforms.Compose(data_augmentation(config))
     transform_test = transforms.Compose(data_augmentation(config, is_train = False))
 
-    train_loader, test_loader = get_data_loader(
-        transform_train, transform_test, config)
+    # 得到可用于torch的DataLoader
+    # get data loader
+    train_loader, test_loader = get_data_loader(transform_train, transform_test, config)
 
-    # start training
-    logger.info("            =======  Training  =======\n")
+    # 得到用于更新lr的函数
+    # get lr scheduler
     lr_scheduler = get_learning_rate_scheduler(optimizer, last_epoch, config)
+
+    # 开始训练
+    # start training network
+    logger.info("            =======  Training  =======\n")
     for epoch in range(last_epoch + 1, config.epochs):
+        # 更新学习率lr
         # adjust learning rate
         if lr_scheduler:
             if config.lr_scheduler.type == 'ADAPTIVE':
@@ -212,7 +237,9 @@ def start_training(work_path, resume = False, config_dict = None):
                 lr_scheduler.step(epoch)
         lr = get_current_lr(optimizer)
         writer.add_scalar('learning_rate', lr, epoch)
+        # train one epoch
         train_step(train_loader, net, criterion, optimizer, epoch, device)
+        # validate network
         if epoch == 0 or (epoch + 1) % config.eval_freq == 0 or epoch == config.epochs - 1:
             test(test_loader, net, criterion, optimizer, epoch, device)
 

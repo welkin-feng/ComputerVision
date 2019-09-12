207b3d6785ffc6651051e5fcfc5ef3fe6d0a49f0
Welkin <382523558@qq.com>
2019-09-12 15:15:32 +0800

    feat: add new modules of detection

--- models/__init__.py
+++ models/__init__.py
@@ -11,13 +11,15 @@ File Name:  __init__.py.py
 __author__ = 'Welkin'
 __date__ = '2019/6/21 08:34'
 
+from .util_modules import *
 from .alexnet import *
 from .inception_v1 import *
 from .vgg import *
 from .spp import *
 from .mobilenet_v1 import *
 from .resnet import *
-
+from .yolo_v1 import *
+from .yolo_v2 import *
 
 def get_model(config):
     return globals()[config.architecture](config.num_classes, config.input_size)

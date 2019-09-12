ad9d4dae5a6f2d75056fb6c325163dd9e0cce574
Welkin <382523558@qq.com>
2019-09-12 15:05:11 +0800

    feat: add `conv_bn_activation` function, creating conv block with specific activation

--- models/util_modules.py
+++ models/util_modules.py
@@ -13,14 +13,21 @@ __date__ = '2019/6/24 12:23'
 
 import torch.nn as nn
 
-__all__ = ['conv_bn_relu']
+__all__ = ['conv_bn_activation', 'conv_bn_relu']
 
 
-def conv_bn_relu(in_channels, out_channels, kernel_size, stride = 1, padding = 0,
-                 use_batch_norm = True, dilation = 1, groups = 1, bias = True):
+def conv_bn_activation(in_channels, out_channels, kernel_size, stride = 1, padding = 0,
+                       use_batch_norm = True, dilation = 1, groups = 1, bias = True, activation = None):
     block = [nn.Conv2d(in_channels, out_channels, kernel_size, stride,
                        padding, dilation, groups, bias)]
     if use_batch_norm:
         block += [nn.BatchNorm2d(out_channels)]
-    block += [nn.ReLU(inplace = True)]
+    if activation is not None:
+        block += [activation]
     return nn.Sequential(*block)
+
+
+def conv_bn_relu(in_channels, out_channels, kernel_size, stride = 1, padding = 0,
+                 use_batch_norm = True, dilation = 1, groups = 1, bias = True):
+    return conv_bn_activation(in_channels, out_channels, kernel_size, stride, padding,
+                              use_batch_norm, dilation, groups, bias, nn.ReLU(inplace = True))

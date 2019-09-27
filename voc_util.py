c4bd94e31ad7c4cda30c38d08224e353f0a2c585
Welkin <382523558@qq.com>
2019-09-27 18:53:59 +0800

    Add param `scale_with_padding` for `VOCTransformResize`, inherit class `VOCDetection` from `torchvision.datasets.VOCDetection`

--- voc_util.py
+++ voc_util.py
@@ -313,30 +313,41 @@ class VOCDetection(torchvision.datasets.VOCDetection):
                  transform = None,
                  target_transform = None,
                  transforms = None):
+        super(torchvision.datasets.VOCDetection, self).__init__(root, transforms, transform, target_transform)
+        DATASET_YEAR_DICT = {'2012': {'base_dir': 'VOCdevkit/VOC2012', 'filename': 'VOCtrainval_11-May-2012.tar'},
+                             '2007': {'base_dir': 'VOCdevkit/VOC2007', 'filename': 'VOCtrainval_06-Nov-2007.tar'}}
+        self.year = year
+        # self.url = DATASET_YEAR_DICT[year]['url']
+        # self.filename = DATASET_YEAR_DICT[year]['filename']
+        # self.md5 = DATASET_YEAR_DICT[year]['md5']
+        # self.image_set = verify_str_arg(image_set, "image_set", ("train", "trainval", "val", "test"))
         valid_values = ("train", "trainval", "val", "test")
         if image_set not in valid_values:
             raise ValueError(f"Unknown value '{image_set}' for argument 'image_set'. Valid values are {valid_values}.")
-        if image_set != "test":
-            super().__init__(root, year, image_set, download, transform, target_transform, transforms)
-        else:
-            super().__init__(root, year, 'test', download, transform, target_transform, transforms)
-            self.image_set = image_set
-            DATASET_YEAR_DICT = {'2012': {'base_dir': 'VOCdevkit/VOC2012'},
-                                 '2007': {'base_dir': 'VOCdevkit/VOC2007'}}
-            base_dir = DATASET_YEAR_DICT[year]['base_dir']
-            voc_root = os.path.join(self.root, base_dir)
-            image_dir = os.path.join(voc_root, 'JPEGImages')
-            annotation_dir = os.path.join(voc_root, 'Annotations')
+        self.image_set = image_set
 
-            splits_dir = os.path.join(voc_root, 'ImageSets/Main')
-            split_f = os.path.join(splits_dir, image_set.rstrip('\n') + '.txt')
+        base_dir = DATASET_YEAR_DICT[year]['base_dir']
+        voc_root = os.path.join(self.root, base_dir)
+        image_dir = os.path.join(voc_root, 'JPEGImages')
+        annotation_dir = os.path.join(voc_root, 'Annotations')
 
-            with open(os.path.join(split_f), "r") as f:
-                file_names = [x.strip() for x in f.readlines()]
+        # if download:
+        #     download_extract(self.url, self.root, self.filename, self.md5)
 
-            self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
-            self.annotations = [os.path.join(annotation_dir, x + ".xml") for x in file_names]
-            assert (len(self.images) == len(self.annotations))
+        if not os.path.isdir(voc_root):
+            raise RuntimeError('Dataset not found or corrupted.' +
+                               ' You can use download=True to download it')
+
+        splits_dir = os.path.join(voc_root, 'ImageSets/Main')
+
+        split_f = os.path.join(splits_dir, image_set.rstrip('\n') + '.txt')
+
+        with open(os.path.join(split_f), "r") as f:
+            file_names = [x.strip() for x in f.readlines()]
+
+        self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
+        self.annotations = [os.path.join(annotation_dir, x + ".xml") for x in file_names]
+        assert (len(self.images) == len(self.annotations))
 
 
 def voc_collate(batch):

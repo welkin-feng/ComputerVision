0df70e2ebdb0512f43710f0a169eec6db1d700d8
Welkin <382523558@qq.com>
2020-02-09 22:04:10 +0800

    Add `Ranger` Optimizer

---
+++ optimizer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Project Name:   ComputerVision 
+
+File Name:  __init__.py.py
+
+"""
+
+__author__ = 'Welkin'
+__date__ = '2020/2/9 21:59'
+
+from .ranger import Ranger

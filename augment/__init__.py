652a93477daccfebed9d113fe8fbe9d9367a477c
Welkin <382523558@qq.com>
2020-02-11 12:39:46 +0800

    Add `AutoAugment`

---
+++ augment/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Project Name:   ComputerVision 
+
+File Name:  __init__.py.py
+
+"""
+
+__author__ = 'Welkin'
+__date__ = '2020/2/11 12:33'
+
+
+from .autoaugment import ImageNetPolicy, CIFAR10Policy, SVHNPolicy

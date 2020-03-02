94d7fa805c308b550a3ff4f9a785196759b5ea1a
Welkin <382523558@qq.com>
2020-03-03 01:01:12 +0800

    Update `resnet`

--- cvmodels/models/resnet.py
+++ cvmodels/models/resnet.py
@@ -13,11 +13,68 @@ __date__ = '2019/7/10 00:23'
 
 import torch.nn as nn
 
-from .resnet_modules import *
-
 __all__ = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
            'resnet20_cifar', 'resnet32_cifar', 'resnet44_cifar',
            'resnet56_cifar', 'resnet110_cifar']
+
+
+class BasicBlock(nn.Module):
+    """  """
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels_reduced, stride = 1, dilation = 1):
+        """ Constructor for ResNet_Block """
+        super().__init__()
+        out_channels = out_channels_reduced * self.expansion
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels_reduced, 3, stride, dilation, dilation),
+            nn.BatchNorm2d(out_channels_reduced),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(out_channels_reduced, out_channels, 3, 1, dilation, dilation),
+            nn.BatchNorm2d(out_channels),
+        )
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, stride),
+            nn.BatchNorm2d(out_channels)
+        ) if stride != 1 or in_channels != out_channels else None
+
+    def forward(self, x):
+        out = self.conv(x)
+        if self.downsample:
+            x = self.downsample(x)
+        out += x
+        return F.relu(out, inplace = True)
+
+
+class Bottleneck(nn.Module):
+    """  """
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels_reduced, stride = 1, dilation = 1):
+        """ Constructor for ResNet_Block """
+        super().__init__()
+        out_channels = out_channels_reduced * self.expansion
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels_reduced, 1, stride),
+            nn.BatchNorm2d(out_channels_reduced),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(out_channels_reduced, out_channels_reduced, 3, 1, dilation, dilation),
+            nn.BatchNorm2d(out_channels_reduced),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(out_channels_reduced, out_channels, 1),
+            nn.BatchNorm2d(out_channels),
+        )
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, stride),
+            nn.BatchNorm2d(out_channels)
+        ) if stride != 1 or in_channels != out_channels else None
+
+    def forward(self, x):
+        out = self.conv(x)
+        if self.downsample:
+            x = self.downsample(x)
+        out += x
+        return F.relu(out, inplace = True)
 
 
 class ResNet(nn.Module):

b6d5557f6c2c5d215a608e0426f3b494d90568b8
Welkin <382523558@qq.com>
2020-03-14 03:02:13 +0800

    Update `GridMask` augmentation

--- cvmodels/augment/grid_mask.py
+++ cvmodels/augment/grid_mask.py
@@ -6,9 +6,12 @@ import math
 
 
 class GridMask(object):
-    def __init__(self, d1, d2, rotate = 1, ratio = 0.5, mode = 0, prob = 1.):
-        self.d1 = d1
-        self.d2 = d2
+    def __init__(self, num_grid, rotate = 0, ratio = 0.5, mode = 0, prob = 1.):
+        if isinstance(num_grid, int):
+            num_grid = (num_grid, num_grid)
+        if isinstance(rotate, int):
+            rotate = (-rotate, rotate)
+        self.num_grid = num_grid
         self.rotate = rotate
         self.ratio = ratio
         self.mode = mode
@@ -28,7 +31,9 @@ class GridMask(object):
         # will be able to cover all the image spot after the rotation. This is also the minimum square.
         hh = math.ceil((math.sqrt(h * h + w * w)))
 
-        d = np.random.randint(self.d1, self.d2)
+        num_grid = np.random.randint(self.num_grid[0], self.num_grid[1] + 1)
+        d = min(h, w) // num_grid
+        # d = np.random.randint(self.d1, self.d2)
         # d = self.d
 
         # maybe use ceil? but i guess no big difference
@@ -49,7 +54,7 @@ class GridMask(object):
             s = max(min(s, hh), 0)
             t = max(min(t, hh), 0)
             mask[:, s:t] *= 0
-        r = np.random.randint(self.rotate)
+        r = np.random.randint(self.rotate[0], self.rotate[1] + 1)
         mask = Image.fromarray(np.uint8(mask))
         mask = mask.rotate(r)
         mask = np.asarray(mask)
@@ -66,24 +71,24 @@ class GridMask(object):
 
 
 class GridMaskBatch(object):
-    def __init__(self, d1, d2, rotate = 1, ratio = 0.5, mode = 0, prob = 1.):
+    def __init__(self, num_grid, rotate = 0, ratio = 0.5, mode = 0, prob = 1.):
         super(GridMaskBatch, self).__init__()
         self.rotate = rotate
         self.ratio = ratio
         self.mode = mode
         self.st_prob = prob
-        self.grid = GridMask(d1, d2, rotate, ratio, mode, prob)
+        self.grid = GridMask(num_grid, rotate, ratio, mode, prob)
 
     def set_prob(self, epoch, max_epoch):
         self.grid.set_prob(epoch, max_epoch)
 
-    def __call__(self, ori_img_batch, img_batch, label_batch):
+    def __call__(self, ori_img_batch, img_batch):
         if np.random.rand() > self.st_prob:
-            return img_batch, label_batch
+            return img_batch
 
         n, c, h, w = ori_img_batch.size()
         y = []
         for i in range(n):
             y.append(self.grid(ori_img_batch[i]))
-        y = torch.cat(y).view(n, c, h, w)
+        y = torch.cat(y).view(n, c, h, w).to(ori_img_batch)
         return y

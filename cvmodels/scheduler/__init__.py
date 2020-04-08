665646d273e92797cc37bb7d4e5f692ef8a0f822
Welkin <382523558@qq.com>
2020-04-09 03:23:58 +0800

    Add `CosineLR` scheduler, update `TanhLR` scheduler

--- cvmodels/scheduler/__init__.py
+++ cvmodels/scheduler/__init__.py
@@ -11,5 +11,6 @@ File Name:  __init__.py.py
 __author__ = 'Welkin'
 __date__ = '2020/2/12 19:37'
 
+from .cosine_lr import CosineLR
 from .tanh_lr import TanhLR
 from .plateau_lr import PlateauLR
--- cvmodels/scheduler/cosine_lr.py
+++ cvmodels/scheduler/cosine_lr.py
@@ -4,16 +4,19 @@
 
 Project Name:   ComputerVision 
 
-File Name:  tanh_lr.py
+File Name:  cosine_lr.py
 
 """
+
+__author__ = 'Welkin'
+__date__ = '2020/4/9 00:56'
 
 import math
 import torch
 from torch.optim.lr_scheduler import _LRScheduler
 
 
-class TanhLR(_LRScheduler):
+class CosineLR(_LRScheduler):
     """
        Hyberbolic-Tangent decay with restarts.
        This is described in the paper https://arxiv.org/abs/1806.01593
@@ -22,8 +25,6 @@ class TanhLR(_LRScheduler):
     def __init__(self,
                  optimizer: torch.optim.Optimizer,
                  t_initial: int,
-                 lower_bound: float = -6.,
-                 upper_bound: float = 4.,
                  t_mul: float = 1.,
                  lr_min: float = 0.,
                  decay_rate: float = 1.,
@@ -36,12 +37,9 @@ class TanhLR(_LRScheduler):
 
         assert t_initial > 0
         assert lr_min >= 0
-        assert lower_bound < upper_bound
         assert cycle_limit >= 0
         assert warmup_t >= 0
         assert warmup_lr_init >= 0
-        self.lb = lower_bound
-        self.ub = upper_bound
         self.t_initial = t_initial
         self.t_mul = t_mul
         self.lr_min = lr_min
@@ -52,7 +50,7 @@ class TanhLR(_LRScheduler):
         self.warmup_prefix = warmup_prefix
         self.t_in_epochs = t_in_epochs
 
-        super(TanhLR, self).__init__(optimizer, last_epoch)
+        super(CosineLR, self).__init__(optimizer, last_epoch)
 
         if self.warmup_t:
             t_v = self.base_lrs if self.warmup_prefix else self.get_lr(self.warmup_t)
@@ -84,9 +82,7 @@ class TanhLR(_LRScheduler):
                 lr_min = self.lr_min * gamma
                 lr_max_values = [v * gamma for v in self.base_lrs]
 
-                tr = t_curr / t_i
-                lrs = [lr_min + 0.5 * (lr_max - lr_min) * (1 - math.tanh(self.lb * (1. - tr) + self.ub * tr))
-                       for lr_max in lr_max_values]
+                lrs = [lr_min + 0.5 * (lr_max - lr_min) * (1 + math.cos(math.pi * t_curr / t_i)) for lr_max in lr_max_values]
             else:
                 lrs = [self.lr_min * (self.decay_rate ** self.cycle_limit) for _ in self.base_lrs]
         return lrs
